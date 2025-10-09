38c503fd650cf489b811f3e2ecc0625774b667d8
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-10-10 01:08:10 +0700

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { reactRouter } from '@react-router/dev/vite';
-import { visualizer } from 'rollup-plugin-visualizer';
-import type { PluginOption } from 'vite';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
-import type { VitePWAOptions } from 'vite-plugin-pwa';
-import { VitePWA } from 'vite-plugin-pwa';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { reactRouter } from "@react-router/dev/vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import type { PluginOption } from "vite";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import type { VitePWAOptions } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const pwaOptions: Partial<VitePWAOptions> = {
   // TODO: enable if you want to enable PWA service worker
   disable: true,
-  registerType: 'autoUpdate',
+  registerType: "autoUpdate",
   manifest: {
-    short_name: 'vite-react-chakra-starter',
-    name: 'Vite React App Template',
-    lang: 'en',
-    start_url: '/',
-    background_color: '#FFFFFF',
-    theme_color: '#FFFFFF',
-    dir: 'ltr',
-    display: 'standalone',
+    short_name: "vite-react-chakra-starter",
+    name: "Vite React App Template",
+    lang: "en",
+    start_url: "/",
+    background_color: "#FFFFFF",
+    theme_color: "#FFFFFF",
+    dir: "ltr",
+    display: "standalone",
     prefer_related_applications: false,
   },
   pwaAssets: {
@@ -31,7 +31,7 @@ const pwaOptions: Partial<VitePWAOptions> = {
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const isCheckDisabled = mode === 'production' || !!process.env.VITEST;
+  const isCheckDisabled = mode === "production" || !!process.env.VITEST;
   return {
     plugins: [
       reactRouter(),
@@ -39,11 +39,12 @@ export default defineConfig(({ mode }) => {
         ? [
             checker({
               typescript: true,
+              biome: true,
             }),
           ]
         : []),
       tsconfigPaths(),
-      visualizer({ template: 'sunburst' }) as unknown as PluginOption,
+      visualizer({ template: "sunburst" }) as unknown as PluginOption,
       VitePWA(pwaOptions),
     ],
     server: {
